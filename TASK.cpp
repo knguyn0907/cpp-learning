49945a147885a1c9e93237a237f72ca6d2b4a925
Khôi Nguyên <knguyn0907@gmail.com>
2025-08-22 00:55:29 +0700

    push-test-cpp-program

---
+++ TASK.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
