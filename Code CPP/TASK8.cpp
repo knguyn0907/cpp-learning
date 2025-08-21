b98fee3353141cbea48e1955b41ee0c73575ffd3
Khôi Nguyên <knguyn0907@gmail.com>
2025-08-22 02:47:42 +0700

    Tổng hiệu tích thương

---
+++ Code CPP/TASK8.cpp
@@ -0,0 +1,21 @@
+//Nhập vào 2 số nguyên, in ra tổng, hiệu, tích, thương(lấy độ chính xác với 4 chữ số) của 2 số đó.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b; cin >> a >> b;
+
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << (long long)a * b << "\n";
+    if(a == 0 || b == 0) cout << "INVALID";
+    else cout << fixed << setprecision(4) << a / b;
+
+    return 0;
+}
