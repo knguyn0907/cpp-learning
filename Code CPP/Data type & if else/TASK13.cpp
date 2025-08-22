87212cb611f5dac63fcc859b3ee336765fca526a
Khôi Nguyên <knguyn0907@gmail.com>
2025-08-22 15:28:12 +0700

    Kiểm tra ngày tháng

---
+++ Code CPP/Data type & if else/TASK13.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t,n; cin >> t >> n;
+
+    if(n > 0 && t >= 1 && t <= 12){
+        if(t == 1 || t ==  3 || t == 5 || t == 7 || t == 8 || t == 10 || t == 12){
+            cout << 31;
+        }
+        else if(t == 4 || t == 6 || t == 9 || t == 11){
+            cout << 30;
+        }
+        else{
+            if((n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))){
+                cout << 29;
+            }
+            else {
+                cout << 28;
+            }
+        }
+    }
+    else{
+        cout << "INVALID\n";
+    }
+
+    return 0;
+}
